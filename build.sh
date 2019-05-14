9c6089f55160c4d5de4ec3b6c72de6d894db759b
Antonio Martinez <jose.antonio.martinez.torres@intel.com>
2019-05-14 14:27:35 -0700

    Adding Dockerfile_dev for multistage building

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # cloudConnector service
 
 echo -e "  \e[2mGo \e[0m\e[94mBuild(ing)...\e[0m"
-CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o ./cloud-connector-service
 
